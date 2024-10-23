0598a2aa56be136d33e6babc4437c1a3f8dec8ab
diovalda <ferrenxx@gmail.com>
2024-10-23 20:03:26 +0700

    feat: add migration

---
+++ database/migrations/2024_10_23_125531_create_bookings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('vehicle_id')->constrained()->onDelete('cascade');
+            $table->foreignId('driver_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('approved_by_1')->nullable()->constrained('users')->onDelete('cascade');
+            $table->foreignId('approved_by_2')->nullable()->constrained('users')->onDelete('cascade');
+            $table->enum('status', ['pending', 'approved_by_1', 'fully_approved', 'rejected'])->default('pending');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->text('purpose')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
